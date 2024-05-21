33171cf0192a3e8ab977a5d91d95cb503f1a6796
ecarrillo99 <74939517+ecarrillo99@users.noreply.github.com>
2024-05-21 11:40:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './Pages/Home';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import SobreMi from './Pages/SobreMi';
 import RedesSociales from './Pages/RedesSociales';
 import Proyectos from './Pages/Proyectos';
@@ -7,15 +7,15 @@ import Contacto from './Pages/Contacto';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
+      <Router >
+        <Routes >
           <Route path="/" element={<Home></Home>}/>
           <Route path="/sobre-mi" element={<SobreMi></SobreMi>}/>
           <Route path="/redes-sociales" element={<RedesSociales></RedesSociales>}/>
           <Route path="/proyectos" element={<Proyectos></Proyectos>}/>
           <Route path="/contacto" element={<Contacto></Contacto>}/>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
